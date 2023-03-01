c7f1c69647590a11d6bf7fd7d374c466ace853cb
NotEmily3203 <linemily62@gmail.com>
2023-03-01 17:21:54 -0500

    added func output

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@
 
 int main()
 {
+  std::cout<<"isDivisibleBy(100, 25): "<<isDivisibleBy(100, 25)<<std::endl
+           <<"isDivisibleBy(34, 11): "<<isDivisibleBy(34, 11)<<std::endl
+           <<"isPrime(2): "<<isPrime(2)<<std::endl
+           <<"isPrime(15): "<<isPrime(15)<<std::endl
+           <<"nextPrime(17): "<<nextPrime(17)<<std::endl
+           <<"countPrimes(2,7): "<<countPrimes(2,7)<<std::endl
+           <<"isTwinPrime(7): "<<isTwinPrime(7)<<std::endl
+           <<"isTwinPrime(23): "<<isTwinPrime(23)<<std::endl
+           <<"nextTwinPrime(37): "<<nextTwinPrime(37)<<std::endl
+           <<"largestTwinPrime(1, 31): "<<largestTwinPrime(1, 31)<<std::endl;
   return 0;
 }
